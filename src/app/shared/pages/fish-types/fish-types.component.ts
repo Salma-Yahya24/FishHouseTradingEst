a6c2c027b164fbf254fbc038c7db0867bd37f937
Salma-Yahya24 <salmayahya1812@gmail.com>
2025-06-28 16:55:03 +0300

    SecondCommit

--- src/app/shared/pages/fish-types/fish-types.component.ts
+++ src/app/shared/pages/fish-types/fish-types.component.ts
@@ -1,10 +1,10 @@
-import { NgFor } from '@angular/common';
+import { CurrencyPipe, NgClass, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-fish-types',
-  imports: [NgFor,FormsModule],
+  imports: [NgFor,FormsModule, CurrencyPipe,NgClass],
   templateUrl: './fish-types.component.html',
   styleUrl: './fish-types.component.scss'
 })
@@ -12,11 +12,32 @@ export class FishTypesComponent {
 searchTerm: string = '';
 
   fishTypes = [
-    { name: 'Tilapia', category: 'Freshwater', stock: 120 },
-    { name: 'Salmon', category: 'Saltwater', stock: 75 },
-    { name: 'Catfish', category: 'Freshwater', stock: 30 },
-    { name: 'Mackerel', category: 'Saltwater', stock: 60 },
-  ];
+  { id: 43, code: '', name: 'جمبري جمبو', price: 1200.00, active: 0 },
+  { id: 44, code: '', name: 'جمبري كبير', price: 0.00, active: 0 },
+  { id: 45, code: '', name: 'جمبري وسط', price: 0.00, active: 0 },
+  { id: 46, code: '', name: 'جمبري خشن', price: 0.00, active: 0 },
+  { id: 47, code: '', name: 'جمبري أبيض', price: 0.00, active: 0 },
+  { id: 48, code: '', name: 'مخلط', price: 0.00, active: 0 },
+  { id: 49, code: '', name: 'حبار أبيض', price: 0.00, active: 0 },
+  { id: 50, code: '', name: 'حبار أحمر', price: 0.00, active: 0 },
+  { id: 51, code: '', name: 'كابوريا', price: 0.00, active: 0 },
+  { id: 52, code: '', name: 'سمك موسى', price: 0.00, active: 0 },
+  { id: 53, code: '', name: 'مرجان', price: 0.00, active: 0 },
+  { id: 54, code: '', name: 'قاص', price: 0.00, active: 0 },
+  { id: 55, code: '', name: 'ناجم', price: 0.00, active: 0 },
+  { id: 56, code: '', name: 'شعور', price: 0.00, active: 0 },
+  { id: 57, code: '', name: 'مكرونة مصري', price: 0.00, active: 0 },
+  { id: 58, code: '', name: 'مكرونة هندي', price: 0.00, active: 0 },
+  { id: 59, code: '', name: 'كمل', price: 0.00, active: 0 },
+  { id: 60, code: '', name: 'بياض', price: 0.00, active: 0 },
+  { id: 61, code: '', name: 'قرش', price: 0.00, active: 0 },
+  { id: 62, code: '', name: 'ديراك', price: 0.00, active: 0 },
+  { id: 63, code: '', name: 'عقام', price: 0.00, active: 0 },
+  { id: 64, code: '', name: 'هامور', price: 0.00, active: 0 },
+  { id: 65, code: '', name: 'مشكل', price: 0.00, active: 0 },
+  { id: 66, code: '66', name: 'يوسف محشي', price: 555.00, active: 1 },
+];
+
 
   filteredFishTypes() {
     return this.fishTypes.filter(fish =>
